0d4b41bffaba0750d7cf020eebeec5c11a77aa0a
Claudio Moscoso <cmoscosoz@gmail.com>
2021-09-21 12:38:20 +1200

    changing name routes

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
+  static const routeName = '/category-meals/';
   // final String categoryID;
   // final String CategoryTitle;
 
